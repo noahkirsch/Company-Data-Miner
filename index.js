ce5f688ce669deddcd4642bd64d9d44ae37b9b47
Noah Kirschbaum <noah.kirschbaum@gmail.com>
2018-08-09 14:50:53 -0700

    Add index.js for process object, so companySearch methods are siloed for testing

---
+++ index.js
@@ -0,0 +1,12 @@
+let fs = require('fs');
+let companySearch = require('./companySearch');
+
+const args = process.argv.slice(2);
+const filePath = args[0];
+const command = args[1];
+const arg = args[2];
+
+const companyData = JSON.parse(fs.readFileSync(filePath, 'utf8'));
+
+//Call Router to route logic to proper function
+companySearch.router(companyData, command, arg);
