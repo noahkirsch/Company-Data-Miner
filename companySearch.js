31c0717a6688e88478f2b8a49528ad0f4b969c15
Noah Kirschbaum <noah.kirschbaum@gmail.com>
2018-08-09 12:37:31 -0700

    Add function for find_before command

--- companySearch.js
+++ companySearch.js
@@ -75,6 +75,23 @@ var find_companies_between_size = (arg) => {
 	}
 }
 
+//Function for find_type command
+var find_type = (arg) => {
+	var output = [];
+
+	companyData.forEach((company) => {
+		if (company.company_category === arg) {
+			output.push(company.company_name);
+		}
+	});
+
+	if (output.length === 0) {
+		console.log("There are no results for the command: '" + command + "', and argument: '" + arg + "', please try again.");
+	} else {
+		console.log('Company Names: \n' + output.join(', ') + '\n\nNumber of Companies: ' + output.length);
+	}
+}
+
 //Routing logic for commands
 if (command === 'locate') {
 	locate(arg);
@@ -86,12 +103,36 @@ if (command === 'locate') {
 	const validSizeArgs = ['1-10', '11-50', '51-200', '201-500', '501-1,000', '1,001-5,000', '5,001-10,000', '10,001+'];
 
 	if (validSizeArgs.includes(arg)) {
-			find_companies_between_size(arg);
+		find_companies_between_size(arg);
 	} else {
-		console.log("Invalid command, please try again using on of the following commands: \n" + validSizeArgs.join('\n'));
+		console.log("Invalid argument, please try again using on of the following arguments: \n" + validSizeArgs.join('\n'));
 	}
 } else if (command === 'find_type') {
+	const validTypeArgs = ['N/A',
+												 'Aerospace and Defense',
+												 'Business & Legal Services',
+												 'Data/Technology',
+												 'Education',
+												 'Energy',
+												 'Environment & Weather',
+												 'Finance & Investment',
+												 'Food & Agriculture',
+												 'Geospatial/Mapping',
+												 'Governance',
+												 'Healthcare',
+												 'Housing/Real Estate',
+												 'Insurance',
+												 'Lifestyle & Consumer',
+												 'Media',
+												 'Research & Consulting',
+												 'Scientific Research',
+												 'Transportation'];
 
+	if (validTypeArgs.includes(arg)) {
+		find_type(arg);
+	} else {
+		console.log("Invalid argument, please be sure to include the argument in quotes and try again using on of the following arguments: \n" + validTypeArgs.join('\n'));
+	}
 } else {
 	console.log("Invalid command, please try again using on of the following commands: \nlocate\nfind_before\nfind_after\nfind_companies_between_size\n")
 }
